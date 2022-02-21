3bca2d125b4daf85e6ebca435c0fd25b75ae5472
Aarush Gupta <aarush@theaarushgupta.com>
2022-02-21 14:08:40 -0800

    feat: scroll if full buffer is not in view

--- oxygen/main.py
+++ oxygen/main.py
@@ -13,25 +13,29 @@ def main() -> None:
     arguments = parser.parse_args()
     buffer = file.load(arguments.file)
     def _main(screen: "curses._CursesWindow") -> None:
-        window_ = window.Window(curses.LINES - 1, curses.COLS - 1)
         cursor_ = cursor.Cursor(buffer)
+        window_ = window.Window(buffer, cursor_, curses.LINES - 1, curses.COLS - 1)
         color_ = color.Color(screen)
         colorscheme = color_.create(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
         color_.apply(colorscheme)
         while True:
             screen.erase()
-            for x, y in enumerate(buffer[:window_.rows]):
-                screen.addstr(x, 0, y[:window_.columns])
-            screen.move(cursor_.row, cursor_.column)
+            for row, line in enumerate(buffer[window_.row:window_.row + window_.rowCount]):
+                screen.addstr(row, 0, line)
+            screen.move(*window_.translate())
             key = screen.getkey()
             if key == "q":
                 sys.exit(0)
             elif key == "KEY_UP":
                 cursor_.up()
+                window_.up()
             elif key == "KEY_DOWN":
                 cursor_.down()
+                window_.down()
             elif key == "KEY_LEFT":
                 cursor_.left()
+                window_.up()
             elif key == "KEY_RIGHT":
                 cursor_.right()
+                window_.down()
     curses.wrapper(_main)
--- oxygen/window.py
+++ oxygen/window.py
@@ -1,4 +1,33 @@
+from oxygen import cursor
+
 class Window:
-    def __init__(self, rows: int, columns: int) -> None:
-        self.rows = rows
-        self.columns = columns
+    def __init__(
+        self,
+        buffer: list,
+        cursor_: cursor.Cursor,
+        rowCount: int,
+        columnCount: int,
+        row: int = 0,
+        column: int = 0
+    ) -> None:
+        self.buffer = buffer
+        self.cursor = cursor_
+        self.rowCount = rowCount
+        self.columnCount = columnCount
+        self.row = row
+        self.column = column
+
+    @property
+    def bottom(self) -> int:
+        return self.row + self.rowCount - 1
+
+    def up(self) -> None:
+        if self.cursor.row == self.row - 1 and self.row > 0:
+            self.row -= 1
+
+    def down(self) -> None:
+        if self.cursor.row == self.bottom + 1 and self.bottom < len(self.buffer) - 1:
+            self.row += 1
+
+    def translate(self) -> tuple:
+        return self.cursor.row - self.row, self.cursor.column - self.column
