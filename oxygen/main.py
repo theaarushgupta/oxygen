b5ed948647b4f9ffd130963c1dadd82719688ba0
Aarush Gupta <aarush@theaarushgupta.com>
2022-02-22 18:21:07 -0800

    feat: refactor main program to become a curses-wrappable class

--- oxygen/main.py
+++ oxygen/main.py
@@ -7,49 +7,54 @@ from oxygen import window
 from oxygen import cursor
 from oxygen import color
 
+class Oxygen:
+    def __init__(self, screen: "curses._CursesWindow", filename: str) -> None:
+        self.screen = screen
+        self.buffer = file.load(filename)
+        self.cursor = cursor.Cursor(self.buffer)
+        self.window = window.Window(self.buffer, self.cursor, curses.LINES - 1, curses.COLS - 1)
+        self.color = color.Color(self.screen)
+        self.color.apply(self.color.create(1))
+        self.entry()
+
+    def entry(self) -> None:
+        while True:
+            self.screen.erase()
+            for row, line in enumerate(self.buffer[self.window.row:self.window.row + self.window.rowCount]):
+                if row == self.cursor.row - self.window.row and self.window.column > 0:
+                    line = "«" + line[self.window.column + 1:]
+                if len(line) > self.window.columnCount:
+                    line = line[:self.window.columnCount - 1] + "»"
+                self.screen.addstr(row, 0, line)
+            self.screen.move(*self.window.translate())
+            key = self.screen.getkey()
+            if key == "q":
+                sys.exit(0)
+            elif key == "KEY_UP":
+                self.cursor.up()
+                self.window.up()
+                self.window.horizontalScroll()
+            elif key == "KEY_DOWN":
+                self.cursor.down()
+                self.window.down()
+                self.window.horizontalScroll()
+            elif key == "KEY_LEFT":
+                self.cursor.left()
+                self.window.up()
+                self.window.horizontalScroll()
+            elif key == "KEY_RIGHT":
+                    self.cursor.right()
+                    self.window.down()
+                    self.window.horizontalScroll()
+            else:
+                self.buffer.insert(self.cursor, key)
+                for _ in key:
+                    self.cursor.right()
+                    self.window.down()
+                    self.window.horizontalScroll()
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
     arguments = parser.parse_args()
-    buffer_ = file.load(arguments.file)
-    def _main(screen: "curses._CursesWindow") -> None:
-        cursor_ = cursor.Cursor(buffer_)
-        window_ = window.Window(buffer_, cursor_, curses.LINES - 1, curses.COLS - 1)
-        color_ = color.Color(screen)
-        colorscheme = color_.create(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        color_.apply(colorscheme)
-        def right() -> None:
-            cursor_.right()
-            window_.down()
-            window_.horizontalScroll()
-        while True:
-            screen.erase()
-            for row, line in enumerate(buffer_[window_.row:window_.row + window_.rowCount]):
-                if row == cursor_.row - window_.row and window_.column > 0:
-                    line = "«" + line[window_.column + 1:]
-                if len(line) > window_.columnCount:
-                    line = line[:window_.columnCount - 1] + "»"
-                screen.addstr(row, 0, line)
-            screen.move(*window_.translate())
-            key = screen.getkey()
-            if key == "q":
-                sys.exit(0)
-            elif key == "KEY_UP":
-                cursor_.up()
-                window_.up()
-                window_.horizontalScroll()
-            elif key == "KEY_DOWN":
-                cursor_.down()
-                window_.down()
-                window_.horizontalScroll()
-            elif key == "KEY_LEFT":
-                cursor_.left()
-                window_.up()
-                window_.horizontalScroll()
-            elif key == "KEY_RIGHT":
-                right()
-            else:
-                buffer_.insert(cursor_, key)
-                for _ in key:
-                    right()
-    curses.wrapper(_main)
+    curses.wrapper(Oxygen, arguments.file)
