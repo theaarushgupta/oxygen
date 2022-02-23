b3dd3c64ad9af40ed1495e3c0152d18263c71fa7
Aarush Gupta <aarush@theaarushgupta.com>
2022-02-22 18:27:30 -0800

    feat: split each action into its own function

--- oxygen/main.py
+++ oxygen/main.py
@@ -17,41 +17,56 @@ class Oxygen:
         self.color.apply(self.color.create(1))
         self.entry()
 
+    def draw(self) -> None:
+        self.screen.erase()
+        for row, line in enumerate(self.buffer[self.window.row:self.window.row + self.window.rowCount]):
+            if row == self.cursor.row - self.window.row and self.window.column > 0:
+                line = "«" + line[self.window.column + 1:]
+            if len(line) > self.window.columnCount:
+                line = line[:self.window.columnCount - 1] + "»"
+            self.screen.addstr(row, 0, line)
+        self.screen.move(*self.window.translate())
+
+    def _exit(self) -> None: sys.exit(0)
+
+    def up(self) -> None:
+        self.cursor.up()
+        self.window.up()
+        self.window.horizontalScroll()
+
+    def down(self) -> None:
+        self.cursor.down()
+        self.window.down()
+        self.window.horizontalScroll()
+
+    def left(self) -> None:
+        self.cursor.left()
+        self.window.up()
+        self.window.horizontalScroll()
+
+    def right(self) -> None:
+        self.cursor.right()
+        self.window.down()
+        self.window.horizontalScroll()
+
+    def insert(self, key: str) -> None:
+        self.buffer.insert(self.cursor, key)
+        for _ in key:
+            self.right()
+
+    def keypress(self) -> None:
+        key = self.screen.getkey()
+        if key == "q": self._exit()
+        elif key == "KEY_UP": self.up()
+        elif key == "KEY_DOWN": self.down()
+        elif key == "KEY_LEFT": self.left()
+        elif key == "KEY_RIGHT": self.right()
+        else: self.insert(key)
+
     def entry(self) -> None:
         while True:
-            self.screen.erase()
-            for row, line in enumerate(self.buffer[self.window.row:self.window.row + self.window.rowCount]):
-                if row == self.cursor.row - self.window.row and self.window.column > 0:
-                    line = "«" + line[self.window.column + 1:]
-                if len(line) > self.window.columnCount:
-                    line = line[:self.window.columnCount - 1] + "»"
-                self.screen.addstr(row, 0, line)
-            self.screen.move(*self.window.translate())
-            key = self.screen.getkey()
-            if key == "q":
-                sys.exit(0)
-            elif key == "KEY_UP":
-                self.cursor.up()
-                self.window.up()
-                self.window.horizontalScroll()
-            elif key == "KEY_DOWN":
-                self.cursor.down()
-                self.window.down()
-                self.window.horizontalScroll()
-            elif key == "KEY_LEFT":
-                self.cursor.left()
-                self.window.up()
-                self.window.horizontalScroll()
-            elif key == "KEY_RIGHT":
-                    self.cursor.right()
-                    self.window.down()
-                    self.window.horizontalScroll()
-            else:
-                self.buffer.insert(self.cursor, key)
-                for _ in key:
-                    self.cursor.right()
-                    self.window.down()
-                    self.window.horizontalScroll()
+            self.draw()
+            self.keypress()
 
 def main() -> None:
     parser = argparse.ArgumentParser()
